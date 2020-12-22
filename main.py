c540ee4e8d1f8435a4b4d81b2a82aae53beb5d73
Didier Pasuy <didierpasuy@users.noreply.github.com>
2020-12-22 16:06:39 -0500

    Correccion en la direccion de Heroku

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from fastapi.middleware.cors import CORSMiddleware
 api = FastAPI()
 
 origins = [
-    "https://byrsapp-vue.herokuapp.com/",
+    "https://byrsapp-vue.herokuapp.com",
     "https://byrsapp-vue.herokuapp.com/:8080",
 ]
 
